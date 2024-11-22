de793335057b6b18d7ec8eecd6760e1a38c373a4
TPautras <tpautras@gmail.com>
2024-11-22 11:04:48 +0100

    add : Dictionnaire

--- Dictionnaire.cs
+++ Dictionnaire.cs
@@ -8,6 +8,79 @@ namespace Boogle_Thomas_Pautras
 {
     internal class Dictionnaire
     {
+        private List<string> dict;
+        private int length;
+        private string lang = "Fançais";
 
+        public Dictionnaire(List<string> dict, int length, string lang)
+        {
+            this.dict = dict;
+            this.length = length;
+            this.lang = lang;
+        }
+
+        public string Lang
+        {
+            get { return lang; }
+        }
+
+        public int Length
+        {
+            get { return length; }
+        }
+
+        public List<string> Dict
+        {
+            get { return dict; }
+        }
+
+        public string toString()
+        {
+            string res "Ce dictionnaire " + this.lang + " contient " + this.length + " : ";
+
+            foreach (string str in this.Dict)
+            {
+                res += str + "\n";
+            }
+            return res;
+        }
+
+        public bool RechDichoRecursif(int start, int end, string wordToFind)
+        {
+            int a = (start + end) / 2;
+
+
+            string find = this.dict[a];
+
+            if (find != wordToFind)
+            {
+                if (end != start)
+                {
+                    if (start < end)
+                    {
+                        if (wordToFind.CompareTo(find) == -1)
+                        {
+                            return RechDichoRecursif(start, a - 1, wordToFind);
+                        }
+                        else
+                        {
+                            return RechDichoRecursif(a + 1, end, wordToFind);
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return true;
+            }
+        }
     }
 }
